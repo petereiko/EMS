86fe040ea0baac16f1021dd85491d29fc6ebf2f7
PeterEikore <PeterEikore@VAT-L-DEV2>
2022-09-15 17:14:47 +0100

    Add project files.

---
+++ EMS/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using EMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EMS.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Create() 
+        {
+            UserViewModel model = new UserViewModel();
+            model.Id = 1;
+            model.Username = "peter";
+            model.PhoneNumber = "07068352430";
+            model.Password = "Password";
+            model.Email = "peter@gmail.com";
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(UserViewModel model)
+        {
+            bool isvalid = ModelState.IsValid;
+            if (!isvalid)
+            {
+
+                foreach (ModelState item in ModelState.Values)
+                {
+                    foreach (ModelError error in item.Errors)
+                    {
+                        model.Errors.Add(error.ErrorMessage);
+                    }
+                }
+
+                return View(model);
+            }
+            //Processing...
+            return View(model);
+        }
+    }
+}
---
+++ EMS/Models/UserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EMS.Models
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        public List<string> Errors { get; set; } = new List<string>();
+    }
+}
